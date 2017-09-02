46f9290358effb5ba414d50c0566d3585f026770
veb3 <vincent.balbarin@yale.edu>
2017-09-02 11:27:17 -0400

    Refactored ftp_profile

--- get_ftp_cli.py
+++ get_ftp_cli.py
@@ -6,6 +6,21 @@ import optparse
 import re
 from bs4 import BeautifulSoup
 
+def ftp_profile(publish_settings):
+  """Takes PublishSettings, extracts ftp user and password, returns ftp command"""
+  soup = BeautifulSoup(publish_settings, 'html.parser')
+
+  profiles = soup.find_all('publishprofile')
+  ftp_profile = [profile for profile in profiles if profile['publishmethod'] == 'FTP'][0]
+
+  matches = re.search('ftp://(.+)/site/wwwroot', ftp_profile['publishurl'])
+
+  host = matches.group(1) if matches else ''
+
+  username = ftp_profile['username'].replace("\\$", "%5C%24")
+  password = ftp_profile['userpwd']
+  return host, username, password, ftp_profile['publishurl']
+
 def main():
   """Executes program and handles options"""
 
@@ -21,18 +36,7 @@ def main():
     infile = open(arguments[0], 'r')
     contents = infile.read()
 
-    soup = BeautifulSoup(contents, 'html.parser')
-
-    profiles = soup.find_all('publishprofile')
-    ftp_profile = [profile for profile in profiles if profile['publishmethod'] == 'FTP'][0]
-
-    matches = re.search('ftp://(.+)/site/wwwroot', ftp_profile['publishurl'])
-
-    ftp_host = matches.group(1) if matches else ''
-
-    ftp_username = ftp_profile['username'].replace("\\$", "%5C%24")
-    ftp_password = ftp_profile['userpwd']
-
+    ftp_host, ftp_username, ftp_password, publish_url = ftp_profile(contents)
     print("")
     print('Execute the following command to connect to the Azure FTP:')
     print("")
@@ -40,7 +44,7 @@ def main():
     print("")
     print('Execute the following command to put the contents of a directory to the Azure website:')
     print("")
-    print('wput $HOME/Sites/[appdir]/* ftp://{}:{}@{}/'.format(ftp_username, ftp_password, ftp_profile['publishurl']))
+    print('wput $HOME/Sites/[appdir]/* ftp://{}:{}@{}/'.format(ftp_username, ftp_password, publish_url))
     print("")
   else:
     p.print_help()

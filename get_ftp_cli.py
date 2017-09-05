b747c18c6afe6527cbc998e46f117bc566551281
veb3 <vincent.balbarin@yale.edu>
2017-09-05 10:13:39 -0400

    Added code to extract MySQL credentials

--- get_ftp_cli.py
+++ get_ftp_cli.py
@@ -7,7 +7,7 @@ import re
 from bs4 import BeautifulSoup
 
 def ftp_profile(publish_settings):
-  """Takes PublishSettings, extracts ftp user and password, returns ftp command"""
+  """Takes PublishSettings, extracts ftp user, password, and host"""
   soup = BeautifulSoup(publish_settings, 'html.parser')
 
   profiles = soup.find_all('publishprofile')
@@ -20,6 +20,20 @@ def ftp_profile(publish_settings):
   username = ftp_profile['username'].replace("\\$", "%5C%24")
   password = ftp_profile['userpwd']
   return host, username, password, ftp_profile['publishurl']
+
+def default_db_connection(publish_settings):
+  """"Takes PublishSettings looks for Azure default db connection, returns default db connection string for local environment and SQL to add user to local db"""
+  
+  username, password = '', ''
+  soup = BeautifulSoup(publish_settings, 'html.parser')
+  connections = soup.find_all('add')
+  regex = 'Database=(.+);Data Source=(.+);User Id=(.+);Password=(.+)'
+  db_connection = [conn for conn in connections if conn['name'] == 'defaultConnection'][0]
+  matches = re.search(regex, db_connection['connectionstring'])
+  if matches:
+    username = matches.group(3)
+    password = matches.group(4)
+  return username, password
 
 def main():
   """Executes program and handles options"""
@@ -37,6 +51,7 @@ def main():
     contents = infile.read()
 
     ftp_host, ftp_username, ftp_password, publish_url = ftp_profile(contents)
+    db_user, db_passwd = default_db_connection(contents)
     print("")
     print('Execute the following command to connect to the Azure FTP:')
     print("")
@@ -46,6 +61,15 @@ def main():
     print("")
     print('wput $HOME/Sites/[appdir]/* ftp://{}:{}@{}/'.format(ftp_username, ftp_password, publish_url))
     print("")
+    print('Append the following line to `./httpd.conf`. (Substitute correct [Local_DB_Name]:')
+    print('')
+    print(
+      '''SetEnv MYSQLCONNSTR_defaultConnection "Database=[Local_DB_Name];Data Source=localhost;User Id={};Password={}"'''.format(db_user, db_passwd))
+    print('')
+    print('Execute the following command to add the user to the local MySQL database:')
+    print('')
+    print(
+      '''mysql -u root -p -e "GRANT ALL PRIVILEGES ON *.* TO '{}'@'localhost' IDENTIFIED BY '{}';"'''.format(db_user, db_passwd))
   else:
     p.print_help()
 
